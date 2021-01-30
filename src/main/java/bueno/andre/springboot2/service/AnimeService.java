a8d1692274e5e314356c750ce3bce1d9cc91578e
Andre Bueno <andre.obueno@gmail.com>
2021-01-30 14:23:30 -0300

    Finished class 8

--- src/main/java/bueno/andre/springboot2/service/AnimeService.java
+++ src/main/java/bueno/andre/springboot2/service/AnimeService.java
@@ -1,15 +1,26 @@
 package bueno.andre.springboot2.service;
 
 import bueno.andre.springboot2.domain.Anime;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
 @Service
 public class AnimeService {
 
+    private List<Anime> animes = List.of(new Anime(1L, "DB2"), new Anime(2L, "Berseke"));
+
     //private final AnimeRepository animeRepository;
-    public List<Anime> listAll(){
-        return List.of(new Anime(1L,"DB2"), new Anime(2L,"Berseke"));
+    public List<Anime> listAll() {
+        return animes;
+    }
+
+    public Anime findById(long id) {
+        return animes.stream()
+                .filter(anime -> anime.getId() == id)
+                .findFirst()
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Anime not found"));
     }
 }
