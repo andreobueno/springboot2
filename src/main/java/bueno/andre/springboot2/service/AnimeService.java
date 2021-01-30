d0698532c0760af8061c0085f1f1050559462e88
Andre Bueno <andre.obueno@gmail.com>
2021-01-30 14:46:51 -0300

    Finished video9

--- src/main/java/bueno/andre/springboot2/service/AnimeService.java
+++ src/main/java/bueno/andre/springboot2/service/AnimeService.java
@@ -5,12 +5,17 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 @Service
 public class AnimeService {
+    private static List<Anime> animes;
 
-    private List<Anime> animes = List.of(new Anime(1L, "DB2"), new Anime(2L, "Berseke"));
+    static {
+        animes = new ArrayList<>(List.of(new Anime(1L, "DB2"), new Anime(2L, "Berseke")));
+    }
 
     //private final AnimeRepository animeRepository;
     public List<Anime> listAll() {
@@ -22,5 +27,11 @@ public class AnimeService {
                 .filter(anime -> anime.getId() == id)
                 .findFirst()
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Anime not found"));
+    }
+
+    public Anime save(Anime anime) {
+        anime.setId(ThreadLocalRandom.current().nextLong(3, 1000000));
+        animes.add(anime);
+        return anime;
     }
 }
