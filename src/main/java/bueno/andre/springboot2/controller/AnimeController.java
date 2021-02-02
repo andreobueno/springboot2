ef386fd43674f00c982abba517efc4ddc1af3c82
Andre Bueno <andre.obueno@gmail.com>
2021-02-02 20:34:51 -0300

    Finished video14

--- src/main/java/bueno/andre/springboot2/controller/AnimeController.java
+++ src/main/java/bueno/andre/springboot2/controller/AnimeController.java
@@ -1,6 +1,8 @@
 package bueno.andre.springboot2.controller;
 
 import bueno.andre.springboot2.domain.Anime;
+import bueno.andre.springboot2.requests.AnimePostRequestBody;
+import bueno.andre.springboot2.requests.AnimePutRequestBody;
 import bueno.andre.springboot2.service.AnimeService;
 import bueno.andre.springboot2.util.DateUtil;
 import lombok.RequiredArgsConstructor;
@@ -31,14 +33,13 @@ public class AnimeController {
     @GetMapping(path = "/{id}")
     public ResponseEntity<Anime> findById(@PathVariable long id) {
         log.info(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
-        return ResponseEntity.ok(animeService.findById(id));
+        return ResponseEntity.ok(animeService.findByIdOrThrowBadRequestException(id));
     }
 
     @PostMapping
     //@ResponseStatus(HttpStatus.CREATED)
-    public ResponseEntity<Anime> save(@RequestBody Anime anime) {
-        return new ResponseEntity<>(animeService.save(anime), HttpStatus.CREATED);
-
+    public ResponseEntity<Anime> save(@RequestBody AnimePostRequestBody animePostRequestBody) {
+        return new ResponseEntity<>(animeService.save(animePostRequestBody), HttpStatus.CREATED);
     }
 
     @DeleteMapping(path = "/{id}")
@@ -48,8 +49,8 @@ public class AnimeController {
     }
 
     @PutMapping
-    public ResponseEntity<Void> replace(@RequestBody Anime anime){
-        animeService.replace(anime);
+    public ResponseEntity<Void> replace(@RequestBody AnimePutRequestBody animePutRequestBody){
+        animeService.replace(animePutRequestBody);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
--- src/main/java/bueno/andre/springboot2/repository/AnimeRepository.java
+++ src/main/java/bueno/andre/springboot2/repository/AnimeRepository.java
@@ -1,9 +1,8 @@
 package bueno.andre.springboot2.repository;
 
 import bueno.andre.springboot2.domain.Anime;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
+public interface AnimeRepository extends JpaRepository<Anime, Long> {
 
-public interface AnimeRepository {
-    List<Anime> listAll();
 }
