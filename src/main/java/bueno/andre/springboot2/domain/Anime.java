9827d7c54024370fd1c9fcae83dee1207fbdeaf3
Andre Bueno <andre.obueno@gmail.com>
2021-02-02 20:11:35 -0300

    Finished video13

--- src/main/java/bueno/andre/springboot2/domain/Anime.java
+++ src/main/java/bueno/andre/springboot2/domain/Anime.java
@@ -2,10 +2,22 @@ package bueno.andre.springboot2.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Anime {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+
     private String name;
 }
